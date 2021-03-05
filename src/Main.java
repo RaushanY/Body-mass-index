46e3c09d071fbed250f1b9804e6a38dc223a461c
RaushanY <raushan4066@gmail.com>
2021-03-05 21:26:45 +0300

    fixed #1

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,11 @@ public class Main {
     public static void main(String[] args) {
         double weight = 75;
         double height = 1.85;
-
-        double bodyMassIndex;
-
-        bodyMassIndex = weight / (height * height);
+        double bodyMassIndex = weight / (height * height);
 
         System.out.println("Bmi for weight " + "kg. ");
         System.out.println("and height" + height + "m");
-        System.out.println("is equivalent to " + bodyMassIndex +".");
-        
+        System.out.println("is equivalent to " + bodyMassIndex + ".");
+
     }
 }
